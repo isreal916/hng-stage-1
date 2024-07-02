480b1c664f71a0131de637483e5253f8cfdf08f9
isreal916 <isrealadeoluwa89@gmail.com>
2024-07-02 18:25:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const currentTime = document.getElementById("time");
+const currentDay = document.getElementById("day");
+
+
+setInterval(() =>{
+  const now = new Date();
+  const day = now.toLocaleDateString('en-US',{weekday:"long"});
+
+  const time = now.toLocaleTimeString('en-US')
+  currentDay.textContent = `${day}`
+  currentTime.textContent =`${time}`;
+},1000)
